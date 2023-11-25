7ae7615d9ab644e76a2ee9fdd8c0051b44e2f9e5
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2023-11-25 15:56:52 +0300

    Add files via upload

---
+++ odev 1-815/odev 1-815/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev_1_815
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lbSayi.Items.Clear();
+            int sayi = 1;
+            while (sayi <= 10)
+            {
+                lbSayi.Items.Add(sayi);
+                sayi++;
+            }
+        }
+    }
+}
